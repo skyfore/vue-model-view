3873db0a4620393afd602499e061907019eb7514
yangtian03 <yangtian03@meituan.com>
2022-09-21 03:18:55 +0800

    feat(pkg): add Tailwind CSS support

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueCompositionApi from '@vue/composition-api'
 
+import './assets/index.css'
+
 Vue.use(VueCompositionApi)
 
 new Vue(App).$mount('#app')
